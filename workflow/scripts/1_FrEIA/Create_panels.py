743fc12adaaf1ee0dfab7efb2a48920b66978130
Todor Gitchev <todor.gitchev@gmail.com>
2024-07-30 17:16:48 +0200

    add --input_trinucleotide and --input_diversity arguments for all samples, thus combining the *_controls and *_cases arguments

--- workflow/scripts/1_FrEIA/Create_panels.py
+++ workflow/scripts/1_FrEIA/Create_panels.py
@@ -15,28 +15,39 @@ import pandas as pd
 
 def argumentparsing():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--input_trinucleotide",
+                        dest="in_tnc",
+                        type=str,
+                        help="The path to the file containing the fragment end trinucleotide proportions "
+                             "for controls and cancer from the results sub-folder .../4_FrEIA/4_Compare/Data. "
+                             " Files must be a .csv file!",
+                        default="Dat_GT__sample.csv")
+    parser.add_argument("--input_diversity",
+                        dest="in_div",
+                        type=str,
+                        help="The path to the file containing the fragment end diversity"
+                             "for controls and cancer from the results sub-folder .../4_FrEIA/4_Compare/Data. "
+                             "Files must be a .csv file!",
+                        default="Dat_GT__MDS_sample.csv")
+
     parser.add_argument("--input_trinucleotide_controls",
                         dest="in_tnc_ctr",
                         type=str,
-                        required=True,
                         help="The path to the file containing the fragment end trinucleotide proportions "
                              "for controls. Files must be a .csv file!")
     parser.add_argument("--input_diversity_controls",
                         dest="in_div_ctr",
                         type=str,
-                        required=True,
                         help="The path to the file containing the fragment end diversity"
                              "for controls. Files must be a .csv file!")
     parser.add_argument("--input_trinucleotide_cases",
                         dest="in_tnc_cas",
                         type=str,
-                        required=True,
                         help="The path to the file containing the fragment end trinucleotide proportions "
                              "for cases. Files must be a .csv file!")
     parser.add_argument("--input_diversity_cases",
                         dest="in_div_cas",
                         type=str,
-                        required=True,
                         help="The path to the file containing the fragment end diversity"
                              "for cases. Files must be a .csv file!")
     parser.add_argument("-o", "--output",
@@ -55,14 +66,29 @@ def median_vector(dat_Df):
 def main():
     args = argumentparsing()  # Parse arguments.
 
-    tnc_ctr_Df = pd.read_csv(args.in_tnc_ctr,
-                             sep=",")  # Read control trinucleotide proportions.
-    div_ctr_Df = pd.read_csv(args.in_div_ctr,
-                             sep=",")  # Read control diversity.
-    tnc_cas_Df = pd.read_csv(args.in_tnc_cas,
-                             sep=",")  # Read case trinucleotide proportions.
-    div_cas_Df = pd.read_csv(args.in_div_cas,
-                             sep=",")  # Read case diversity.
+    try:
+        tnc_Df = pd.read_csv(args.in_tnc,
+                             sep=",")  # Read all samples trinucleotide proportions.
+        div_Df = pd.read_csv(args.in_div,
+                             sep=",")  # Read all samples diversity.
+
+        tnc_ctr_Df = tnc_Df[tnc_Df['WhichGroup'] == 'control'].rename(columns={"WhichSample": "sample_name",
+                                                                               "WhichGroup": "group"})
+        tnc_cas_Df = tnc_Df[tnc_Df['WhichGroup'] == 'cancer'].rename(columns={"WhichSample": "sample_name",
+                                                                              "WhichGroup": "group"})
+
+        div_ctr_Df = div_Df[div_Df['group'] == 'control']
+        div_cas_Df = div_Df[div_Df['group'] == 'cancer']
+
+    except OSError:
+        tnc_ctr_Df = pd.read_csv(args.in_tnc_ctr,
+                                 sep=",")  # Read control trinucleotide proportions.
+        div_ctr_Df = pd.read_csv(args.in_div_ctr,
+                                 sep=",")  # Read control diversity.
+        tnc_cas_Df = pd.read_csv(args.in_tnc_cas,
+                                 sep=",")  # Read case trinucleotide proportions.
+        div_cas_Df = pd.read_csv(args.in_div_cas,
+                                 sep=",")  # Read case diversity.
 
     # Merge trinucleotides with diversity.
     ctr_Df = tnc_ctr_Df.merge(div_ctr_Df)
